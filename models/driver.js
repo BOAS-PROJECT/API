c03e4ab701c9309b72f6370be0c38e6704e527ab
rubensalban <rubensalban@gmail.com>
2024-10-26 18:18:16 +0100

    Correctif creation deu proprio

--- models/driver.js
+++ models/driver.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.Owner, { foreignKey: 'ownerId', as: 'owner' });
     }
   }
   Driver.init({
