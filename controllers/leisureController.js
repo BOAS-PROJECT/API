5333a2e614de139857373273a041e8efef465f66
rubensalban <rubensalban@gmail.com>
2024-12-31 17:07:45 +0100

    Reservation du loisir

--- controllers/leisureController.js
+++ controllers/leisureController.js
@@ -1,4 +1,4 @@
-const { Leisure, LeisureImage } = require("../models");
+const { Leisure, LeisureImage, Reservation } = require("../models");
 const jwt = require("jsonwebtoken");
 const admin = require("firebase-admin");
 const { appendErrorLog } = require("../utils/logging");
@@ -45,6 +45,114 @@ const list = async (req, res) => {
     }
 }
 
+const reservation = async (req, res) => {
+  try {
+    const token = req.headers.authorization;
+    const { leisureId, date } = req.body;
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ status: "error", message: "Token non fourni." });
+    }
+
+    if (!date) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "La date est obligatoire." });
+    }
+
+    if (!leisureId) {
+      return res
+        .status(400)
+        .json({
+          status: "error",
+          message: "ID du loisir est obligatoire.",
+        });
+    }
+
+    // Vérifie si l'en-tête commence par "Bearer "
+    if (!token.startsWith("Bearer ")) {
+      return res.status(401).json({
+        status: "error",
+        message: "Format de token invalide.",
+      });
+    }
+
+    // Extrait le token en supprimant le préfixe "Bearer "
+    const customToken = token.substring(7);
+    let decodedToken;
+
+    try {
+      decodedToken = jwt.verify(customToken, process.env.JWT_SECRET);
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res
+          .status(401)
+          .json({ status: "error", message: "TokenExpiredError" });
+      }
+      return res
+        .status(401)
+        .json({ status: "error", message: "Token invalide." });
+    }
+
+    if (!decodedToken) {
+      return res
+        .status(401)
+        .json({ status: "error", message: "Token invalide." });
+    }
+
+    const customerId = decodedToken.id;
+    const customer = await User.findByPk(customerId);
+    if (!customer) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "Le client n'existe pas." });
+    }
+
+    const leisure = await Leisure.findByPk(leisureId);
+    if (!leisure) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "Le loisir n'existe pas." });
+    }
+
+    await Reservation.create({
+      userId: customer.id,
+      leisureId: leisureId,
+      paymentMethodId: 1,
+      date,
+      status: 1,
+    });
+
+    if (customer.token) {
+      const userToken = customer.token;
+      const message = {
+        token: userToken,
+        notification: {
+          title: "Félicitations!",
+          body: `Votre reservation de ${leisure.title} à bien ete prise en compte.`,
+        },
+      };
+      await admin.messaging().send(message);
+    }
+
+    return res.status(201).json({
+      status: "success",
+      message:
+        `La réservation de ${leisure.title} à bien été prise en compte avec succès. Vous serez contacté par l'agence BOAS Service pour confirmer votre reservation. Merci de votre confiance !`,
+    });
+  } catch (error) {
+    console.error(`ERROR RESERVATION CARMOVING: ${error}`);
+    appendErrorLog(`ERROR RESERVATION CARMOVING: ${error}`);
+    return res.status(500).json({
+      status: "error",
+      message: "Une erreur s'est produite lors de la creation du compte.",
+    });
+  }
+};
+
 module.exports = {
-    list
+    list,
+    reservation,
 };
--- routes/leisureRoutes.js
+++ routes/leisureRoutes.js
@@ -5,4 +5,7 @@ const router = express.Router();
 // LISTE DES LOISIRS
 router.get("/list", leisureController.list);
 
+// RESERVATION D'UN LOISIR
+router.post("/reservation", leisureController.reservation);
+
 module.exports = router;
